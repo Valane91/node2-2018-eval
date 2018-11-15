9797cdb8713e62c97da14cd7fbfe639201413317
kamekame90 <lna.exe@gmail.com>
2018-11-15 12:14:42 +0100

    time-server ok

--- time-server.js
+++ time-server.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const app = express();
-
+const moment = require('moment');
 
 app.get('/', (req, res) => {
-//  res.sendFile(__dirname + '/public/index.html')
-res.write('<h1>TEST</h1>')
+  let now = moment().local().format('hh:mm:ss')
+  var contype = req.headers['accept'];
+   if (!contype || contype.indexOf('application/json') !== 0){
+     res.send('<h1 style= "color:blue">'+now+'</h1>');
+   }else{
+     res.json({time:now})
+   }
 })
 
 app.use('/client',
   express.static(__dirname + '/public')
 )
-
-
 
 app.listen(4000, () => {
     console.log('Server is listenning on localhost:4000');
